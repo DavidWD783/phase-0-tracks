17289a6943233c2fc8f43306397a1a5ff4249fdc
David Dolata <dolatada@gmail.com>
2016-08-03 13:33:44 -0700

    adds questions for release 1

---
+++ ruby/vampires.rb
@@ -0,0 +1,14 @@
+# Interview with the Vampire
+# Ask alleged their name.
+puts "What is your name?"
+
+# Ask alleged their age and year of birth.
+puts "What is your age?  What is your year of birth?"
+
+# Ask alleged about garlic bread.
+puts "Would you like some garlic bread?"
+
+# Ask alleged about enrolling in health insurance plan.
+puts "Would you like to enroll in our health insurance?"
+
+# End release 1 questions.
