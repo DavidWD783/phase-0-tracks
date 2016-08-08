996dee75d7c8bde639e2d55a53bf8fc10c6090b4
David Dolata <dolatada@gmail.com>
2016-08-08 11:55:56 -0700

    adds calls with puts

--- ruby/method_detective.rb
+++ ruby/method_detective.rb
@@ -4,44 +4,44 @@
 # When there's more than one suspect who could have
 # committed the crime, add additional calls to prove it.
 
-"iNvEsTiGaTiOn".swapcase
+puts "iNvEsTiGaTiOn".swapcase
 # => “InVeStIgAtIoN”
 
-"zom".gsub(/o/, 'oo')
-"zom".sub(/o/, 'oo')
-"zom".insert(2, 'o')
+puts "zom".gsub(/o/, 'oo')
+puts "zom".sub(/o/, 'oo')
+puts "zom".insert(2, 'o')
 # => “zoom”
 
-"enhance".center(15)
+puts "enhance".center(15)
 # => "    enhance    "
 
-"Stop! You’re under arrest!".upcase
+puts "Stop! You’re under arrest!".upcase
 # => "STOP! YOU’RE UNDER ARREST!"
 
-"the usual" << " suspects"
-"the usual".gsub(/the usual/, 'the usual suspects')  #not a good choice...but it works
-"the usual".insert(-1, ' suspects') #timely to count indices
-"the usual".ljust(18, ' suspects') #timely to count indices
+puts "the usual" << " suspects"
+puts "the usual".gsub(/the usual/, 'the usual suspects')  #not a good choice...but it works
+puts "the usual".insert(-1, ' suspects') #timely to count indices
+puts "the usual".ljust(18, ' suspects') #timely to count indices
 #=> "the usual suspects"
 
-" suspects".prepend("the usual")
-" suspects".gsub(/ /, 'the usual ')
+puts " suspects".prepend("the usual")
+puts " suspects".gsub(/ /, 'the usual ')
 # => "the usual suspects"
 
-"The case of the disappearing last letter".chop
-"The case of the disappearing last letter".chomp("r")
+puts "The case of the disappearing last letter".chop
+puts "The case of the disappearing last letter".chomp("r")
 # => "The case of the disappearing last lette"
 
-"The mystery of the missing first letter".slice(1..-1)
-"The mystery of the missing first letter"[1..-1]
+puts "The mystery of the missing first letter".slice(1..-1)
+puts "The mystery of the missing first letter"[1..-1]
 # => "he mystery of the missing first letter"
 
-"Elementary,    my   dear        Watson!".squeeze(" ")
+puts "Elementary,    my   dear        Watson!".squeeze(" ")
 # => "Elementary, my dear Watson!"
 
-"z".getbyte(0)
+puts "z".getbyte(0)
 # => 122
 # (What is the significance of the number 122 in relation to the character z?) - this is ASCII code index!
 
-"How many times does the letter 'a' appear in this string?".count("a")
+puts "How many times does the letter 'a' appear in this string?".count("a")
 # => 4
