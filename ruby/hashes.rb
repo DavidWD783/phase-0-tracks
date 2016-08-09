8b7f94d8c3471e6d165adf4023ccb54d69d7c1eb
David Dolata <dolatada@gmail.com>
2016-08-09 09:21:08 -0700

    adds pseudocode as outline with puts statements

---
+++ ruby/hashes.rb
@@ -0,0 +1,19 @@
+#Prompt the designer for name
+puts "What is the client's name?"
+
+#Prompt the designer for age
+puts "What is the client's age?"
+
+#Prompt for number of children
+puts "How many children does the client have?"
+
+#Prompt for decor theme
+puts "What is the decor theme for the client?"
+
+#Convert input to appropriate data type
+
+#Print hash back to screen when finished
+
+#Allow user to update a key
+
+#Print latest version of hash, then exit program.
