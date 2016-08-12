cbb3d19a5f412c2a07e6e1a51cb9d70016fece43
David Dolata <dolatada@gmail.com>
2016-08-11 18:48:31 -0700

    adds .each and .map for release 1

--- ruby/iteration.rb
+++ ruby/iteration.rb
@@ -3,3 +3,30 @@ def take_block(input)
   yield(input)
 end
 take_block("input") { |user_input| puts "The user input is #{user_input}." }
+
+#Release 1
+country_array = %w{China India Japan USA}
+puts "Before .each"
+p country_array
+
+# .each method on array
+country_array.each { |country| puts "#{country} is a great place to visit!" }
+puts "After .each"
+p country_array
+
+# create hash from countries/cities
+city_hash = {China: "Beijing", India: "Kerala", Japan: "Osaka", USA: "Seattle"}
+puts "Before .each"
+p city_hash
+
+# .each method on hash
+city_hash.each { |country, city| puts "#{city}, #{country} is a beautiful place to visit!" }
+puts "After .each"
+p city_hash
+
+puts "Before .map!"
+p country_array
+# call .map! on country_array
+country_array.map! { |country| country.upcase }
+puts "After .map!"
+p country_array
