a94b508fccae5cceaeff0581338ce77ae658cec4
David Dolata <dolatada@gmail.com>
2016-08-11 19:13:09 -0700

    adds keep_if and select methods

--- ruby/iteration.rb
+++ ruby/iteration.rb
@@ -1,3 +1,4 @@
+=begin
 def take_block(input)
   puts "Before block status message."
   yield(input)
@@ -30,3 +31,34 @@ p country_array
 country_array.map! { |country| country.upcase }
 puts "After .map!"
 p country_array
+=end
+
+# iterate through items, delete any that are < 4
+arr = [1, 2, 3, 4, 5]
+puts "Before .delete_if:"
+p arr
+arr.delete_if { |number| number < 4 }
+puts "After delete_if:"
+p arr
+
+hash = {Christian: 25, David: 33, Mary: 70, Bob: 49}
+puts "Has before:"
+p hash
+hash.delete_if { |name, age| age > 50 }
+puts "Has after:"
+p hash
+
+# filters data for only items that do satisfy
+arr_alphabet = ["a", "b", "c", "C"]
+puts "Has before:"
+p arr_alphabet
+arr_alphabet.keep_if { |char| char == "c" }
+puts "Has after:"
+p arr_alphabet
+
+hash_alpha_index = {a: 0, b: 1, c: 2, d: 3}
+puts "Has before:"
+p hash_alpha_index
+hash_alpha_index.select! { |letter, index| index > 1 }
+puts "Has after:"
+p hash_alpha_index
