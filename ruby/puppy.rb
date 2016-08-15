f239f7bf00a096b02941c4fb86f8bbce69272578
David Dolata <dolatada@gmail.com>
2016-08-15 10:56:39 -0700

    adds puppy instance with characteristics and behaviors

---
+++ ruby/puppy.rb
@@ -0,0 +1,25 @@
+#Species
+#dog
+
+#Instance
+#puppy
+
+#Characteristics
+#cute: yes
+#likes attention: yes
+#color: varies
+#name: varies
+#tail: yes
+#leg count: 4
+#eye count: 2
+#ear count: 2
+
+
+#Behavior
+#barks
+#whines
+#whimper
+#jumps
+#runs
+#begs
+#licks
