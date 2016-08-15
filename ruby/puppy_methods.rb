8de3e8524a0846ce7e25b783607ec12a76b55f25
David Dolata <dolatada@gmail.com>
2016-08-15 16:27:02 -0700

    adds Language class with methods and 50 times loop

--- ruby/puppy_methods.rb
+++ ruby/puppy_methods.rb
@@ -7,3 +7,31 @@ class Puppy
 
 end
 
+spot = Puppy.new
+#spot.fetch("ball")
+
+class Language
+  def initialize
+    puts "Initialized a new instance of Language..."
+  end
+
+  def verbs(input)
+    puts "Verbs are tough to learn..."
+  end
+
+  def nouns(input)
+    puts "Nouns are also confusing"
+  end
+end
+English = Language.new
+#English.verbs("swimming")
+
+arr = []
+50.times.each do |index|
+  arr.push(Language.new)
+end
+p arr[3]
+  arr.each do |index|
+    index.verbs("swimming")
+    index.nouns("people")
+  end
