e48a50e181a84e33ed5cdcc50ba6b1a007c5522e
David Dolata <dolatada@gmail.com>
2016-08-17 12:49:14 -0700

    adds methods for removing items, updating list, and printing list along with driver code

--- ruby/gps2_2.rb
+++ ruby/gps2_2.rb
@@ -26,19 +26,52 @@ def add_item(hash)
   hash[item_input.to_sym] = 1
   hash
 end
-add_item(list_hash)
+#add_item(list_hash)
 
 # Method to remove an item from the list
 # input: item name, hash
 # steps: prompt the user for item to remove, remove item from hash
 # output: updated hash
 
+def remove_item(hash)
+  puts "Which grocery item would you like to remove from the list?"
+  item_input = gets.chomp.downcase
+
+  hash.delete(item_input.to_sym)
+  hash
+end
+
+
 # Method to update the quantity of an item
 # input: item, quantity, hash
 # steps: prompt user for item and quantity to update, find existing item and update quantity
 # output: updated hash
 
+def update_list(hash)
+  puts "Which grocery item would you like to update?"
+  item_input = gets.chomp.downcase
+
+  puts "What is the new quantity for this item?"
+  item_quantity = gets.chomp.to_i
+
+  hash[item_input.to_sym] = item_quantity
+  hash
+end
+
 # Method to print a list and make it look pretty
 # input: hash
 # steps: print item and quantity statement
 # output: display of print statements
+
+def print_list(hash)
+  hash.each {|item, quantity| puts "You have #{quantity} of #{item} on your list."}
+end
+
+
+### DRIVER CODE ###
+p list = "apples oranges mangoes bread"
+p list_hash = create_list(list)
+p add_item(list_hash)
+p remove_item(list_hash)
+p update_list(list_hash)
+p print_list(list_hash)
