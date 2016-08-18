ac55ab50ddd273045afd780df1a0bd56691f3d7c
David Dolata <dolatada@gmail.com>
2016-08-18 14:08:05 -0700

    adds Shout module with classes and module/method mixin calls

--- ruby/shout.rb
+++ ruby/shout.rb
@@ -1,3 +1,5 @@
+#comment out old code
+=begin
 module Shout
   #methods go here
   def self.yell_angrily(words)
@@ -15,3 +17,27 @@ p Shout.yell_angrily("meanie")
 
 #call and print yell_happily
 p Shout.yell_happily("happy")
+=end
+
+module Shout
+  def potty_mouth(words)
+  puts "Please, keep #{words} to yourself!"
+  end
+end
+
+class Sister
+  include Shout
+end
+
+class Bro
+  include Shout
+end
+
+##DRIVER CODE##
+#call sister class and Shout module
+sister = Sister.new
+sister.potty_mouth("shut-up!")
+
+#call bro class and Shout module
+bro = Bro.new
+bro.potty_mouth("oh, bro")
