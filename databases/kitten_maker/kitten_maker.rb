698cc8bd435ad6be607be461464d79354b6372e3
David Dolata <dolatada@gmail.com>
2016-08-31 16:01:27 -0700

    adds no changes to files

--- databases/kitten_maker/kitten_maker.rb
+++ databases/kitten_maker/kitten_maker.rb
@@ -8,7 +8,7 @@ require 'faker'
 
 # create SQLite3 database
 db = SQLite3::Database.new("kittens.db")
-db.results_as_hash = true
+#db.results_as_hash = true
 
 # learn about fancy string delimiters
 create_table_cmd = <<-SQL
@@ -20,25 +20,24 @@ create_table_cmd = <<-SQL
 SQL
 
 # create a kittens table (if it's not there already)
-db.execute(create_table_cmd)
+#db.execute(create_table_cmd)
 
 # add a test kitten
-# db.execute("INSERT INTO kittens (name, age) VALUES ('Bob', 10)")
+#db.execute("INSERT INTO kittens (name, age) VALUES ('Bob', 10)")
 
 # add LOOOOTS of kittens!
-# so. many. kittens. 
+# so. many. kittens.
 #KittenExplosion
 def create_kitten(db, name, age)
   db.execute("INSERT INTO kittens (name, age) VALUES (?, ?)", [name, age])
 end
 
-10000.times do
+10.times do
   create_kitten(db, Faker::Name.name, 0)
 end
 
 # explore ORM by retrieving data
-# kittens = db.execute("SELECT * FROM kittens")
-# kittens.each do |kitten|
+#kittens = db.execute("SELECT * FROM kittens")
+#kittens.each do |kitten|
 #  puts "#{kitten['name']} is #{kitten['age']}"
-# end
-
+#end
