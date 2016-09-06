2a1032f8afed0bb1836b53e9b8421aa4808b59a3
David Dolata <dolatada@gmail.com>
2016-09-06 15:34:05 -0700

    adds animate calls and moves div around screen

--- web_dev/jquery/script.js
+++ web_dev/jquery/script.js
@@ -9,7 +9,20 @@ else {
 
 //check simple jQuery function call to hide a p element
 $(document).ready(function(){
-    $("button").click(function(){
+    $("#btn-1").click(function(){
         $("p").hide();
+    });
+});
+
+//add some calls on things like fade, animate, etc...
+//add call to animate first the width, then the height, then move to the right...
+$(document).ready(function(){
+    $("#btn-2").click(function(){
+        $("#btn-block").animate({width: '10px'});
+        $("#btn-block").animate({height: '50px'});
+        $("#btn-block").animate({left: '250px'});
+        $("#btn-block").animate({left: '0px'});
+        $("#btn-block").animate({width: '150px'});
+        $("#btn-block").animate({height: '150px'});
     });
 });
