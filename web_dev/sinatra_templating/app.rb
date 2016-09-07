a2e5ad9cf69435750f5bd79c2398db6bcdd25d51
David Dolata <dolatada@gmail.com>
2016-09-07 15:49:12 -0700

    adds campuses table and campuses.erb

--- web_dev/sinatra_templating/app.rb
+++ web_dev/sinatra_templating/app.rb
@@ -1,10 +1,30 @@
 # require gems
 require 'sinatra'
 require 'sqlite3'
+#require_relative 'sql_schema'
 
 set :public_folder, File.dirname(__FILE__) + '/static'
 
 db = SQLite3::Database.new("students.db")
+SQL_CREATE_CAMPUSES = <<-SQL
+CREATE TABLE IF NOT EXISTS campuses (
+    id INTEGER PRIMARY KEY,
+    name VARCHAR(255)
+  )
+SQL
+
+SQL_CREATE_CAMPUSES_STUDENTS = <<-SQL
+  CREATE TABLE IF NOT EXISTS campuses_students (
+    id INTEGER PRIMARY KEY,
+    student_id INTEGER,
+    campus_id INTEGER,
+    FOREIGN KEY (student_id) REFERENCES students(id),
+    FOREIGN KEY (campus_id) REFERENCES campuses(id)
+  )
+SQL
+
+db.execute(SQL_CREATE_CAMPUSES)
+db.execute(SQL_CREATE_CAMPUSES_STUDENTS)
 db.results_as_hash = true
 
 # show students on the home page
@@ -35,4 +55,21 @@ get '/students/:id' do
   @student = db.execute("SELECT * FROM students WHERE id=?", params[:id].to_i)[0]
   puts @student
   erb :student_details
+end
+
+#create new route for new campus
+
+get '/campus/new' do
+  erb :new_campus
+end
+
+#insert new campus into campuses table
+post '/campus' do
+  db.execute("INSERT INTO campuses (name) VALUES (?)", [params['name']])
+  redirect '/campuses'
+end
+
+get '/campuses' do
+  @campuses = db.execute("SELECT * FROM campuses")
+  erb :campuses
 end
