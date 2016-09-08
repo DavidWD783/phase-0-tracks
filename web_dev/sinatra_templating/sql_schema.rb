d4bdbcd9380f3a6fe59d59d55841a1a6585a327a
David Dolata <dolatada@gmail.com>
2016-09-08 10:51:25 -0700

    adds styling for home with table and sql schema page

--- web_dev/sinatra_templating/sql_schema.rb
+++ web_dev/sinatra_templating/sql_schema.rb
@@ -1,12 +1,12 @@
 
-SQL_CREATE_CAMPUSES <<-SQL
+SQL_CREATE_CAMPUSES = <<-SQL
 CREATE TABLE IF NOT EXISTS campuses (
     id INTEGER PRIMARY KEY,
     name VARCHAR(255)
   )
 SQL
 
-SQL_CREATE_CAMPUSES_STUDENTS <<-SQL
+SQL_CREATE_CAMPUSES_STUDENTS = <<-SQL
   CREATE TABLE IF NOT EXISTS campuses_students (
     id INTEGER PRIMARY KEY,
     student_id INTEGER,
@@ -16,3 +16,32 @@ SQL_CREATE_CAMPUSES_STUDENTS <<-SQL
   )
 SQL
 
+SQL_ALL_STUDENTS = <<-SQL
+  SELECT * FROM students
+  SQL
+
+SQL_INSERT_STUDENTS = <<-SQL
+  INSERT INTO students (name, campus, age) VALUES (?,?,?)
+  SQL
+
+SQL_SELECT_ONE_STUDENT = <<-SQL
+  SELECT * FROM students WHERE id=?
+  SQL
+
+SQL_INSERT_CAMPUS = <<-SQL
+  INSERT INTO campuses (name) VALUES (?)
+  SQL
+
+SQL_ALL_CAMPUSES = <<-SQL
+  SELECT * FROM campuses
+  SQL
+
+SQL_INSERT_CAMPUS_STUDENT = <<-SQL
+  INSERT INTO campuses_students (student_id, campus_id) VALUES (?,?)
+  SQL
+
+SQL_ALLOCATIONS = <<-SQL
+    SELECT s.name student, c.name campus FROM campuses c, students s, campuses_students cs
+      WHERE c.id = cs.campus_id
+      AND   cs.student_id = s.id
+  SQL
